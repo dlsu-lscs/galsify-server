2588ec4bb34f74a434def5b5910b863d2deba665
sampdevi <dev86.sean@gmail.com>
2024-04-10 16:22:50 +0800

    preparation for github username migration

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
