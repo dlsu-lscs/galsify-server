650c8731a89fc67929ad28a8ec559f969e0041a8
Sean Denzel Y. Robenta (zel) <38722112+zelkim@users.noreply.github.com>
2024-04-15 10:45:05 +0800

    removed wtf comments by maxy

--- app.js
+++ app.js
@@ -26,5 +26,3 @@ app.use('/auth', auth);
 app.listen(process.env.PORT || 3000, () => {
   console.log(`[GALZify] Server started. Listening on port ${process.env.PORT || 3000}`);
 });
-
-//potngina mo potangin mo potangina mo potangina mo potangina mo poangina mo potangina mo potangina mo potangina mo otangina mo potagnina mo potangina potangina mo potangina potnagina potangina mo potangina mo potangina mo potangina mpo potangin mo potangina mo potanina mo potangina mo potangina mo potangina mo potangian mo po tangina mo potangina mo potangina mo potangina mo otangina mo potangina mo potangina mo otangina mo potangina mo potangina mo potangina mo potangina mo potangina mo potangina mo potangina mo potangina mo potangina mo potangina mo potangina mo potangian mao potangina mo potangina mo potangina mo potangina mo optangina mo potangian mo potangina mo potangina mo potangina mo potangina mo optangina mo potangina mo potangina mo otangina mo potangina mop otangina mpotangina mo potangina mo potangina mop op mo potangian mop potangina mo otangina mop otangina mo potangin mo potngina mo optangia o 
